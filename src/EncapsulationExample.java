7bac866ecc051104b5738af56355c8cbdf64ae4e
Ahmad <shaikhahmad585@gmail.com>
2026-07-21 22:16:55 +0530

    Encapsulation example

---
+++ src/EncapsulationExample.java
@@ -0,0 +1,40 @@
+class Student{
+    private Integer id;
+    private String name;
+
+    Student(Integer id , String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFullName(){
+        return "Id: "+ id + " Name: "+ name;
+    }
+}
+
+
+
+public class EncapsulationExample {
+    public static void main(String[] args){
+        Student s1 = new Student(1,"Ahmad");
+        String s = s1.getFullName();
+        System.out.println(s);
+
+    }
+}
